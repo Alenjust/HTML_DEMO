9b9752bd56e4bf4a43ae17121d64f956bc81be53
ChenAlenjust <63031603+Alenjust@users.noreply.github.com>
2022-07-01 15:57:28 +0800

    Add files via upload

---
+++ css动态登录页面/app.js
@@ -0,0 +1,13 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+	//通过classList属性给对应的Dom添加css类名
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+	// 移除对应的css类名
+  container.classList.remove("sign-up-mode");
+});
